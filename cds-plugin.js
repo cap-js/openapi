160a50a51127e79e35f5d49f1731e0f5f23e10d5
Jeevitha P <jeevitha.p@sap.com>
2024-04-18 17:12:30 +0530

    test

--- cds-plugin.js
+++ cds-plugin.js
@@ -1,14 +1,22 @@
-const cds = module.exports = require('@sap/cds/lib');
+// const cds = module.exports = require('@sap/cds/lib');
 
-class OpenAPI {
+// class OpenAPI {
   
-    get compile() {
-      let compile = require('@sap/cds/lib/compile/cds-compile')
-      cds.extend (compile.to.constructor) .with (class {
-        get openapi() { return super.openapi = require('./lib/compile') }
-      })
-      return super.compile = compile
-    }
-}
+//     get compile() {
+//       let compile = require('@sap/cds/lib/compile/cds-compile')
+//       cds.extend (compile.to.constructor) .with (class {
+//         get openapi() { return super.openapi = require('./lib/compile') }
+//       })
+//       return super.compile = compile
+//     }
+// }
 
-cds.extend (cds.constructor) .with (OpenAPI);
+// cds.extend (cds.constructor) .with (OpenAPI);
+
+// require('./lib/compile/api').registerCompileTargets()
+const cds = require('@sap/cds')
+require('./lib/compile/api').registerCompileTargets()
+const defaults = { path: '/openapi', impl: '@cap-js/openapi' }
+const protocols = cds.env.protocols ??= {}
+protocols.openapi ??= {}
+protocols.openapi = { ...defaults, ...protocols.openapi}
