a3f1121e83aa87861c007fcb93c9e1d26e7999e5
Jeevitha P <jeevitha.p@sap.com>
2024-04-19 15:20:07 +0530

    Update cds-plugin.js

--- cds-plugin.js
+++ cds-plugin.js
@@ -1,18 +1,1 @@
-// const cds = module.exports = require('@sap/cds/lib');
-
-// class OpenAPI {
-  
-//     get compile() {
-//       let compile = require('@sap/cds/lib/compile/cds-compile')
-//       cds.extend (compile.to.constructor) .with (class {
-//         get openapi() { return super.openapi = require('./lib/compile') }
-//       })
-//       return super.compile = compile
-//     }
-// }
-
-// cds.extend (cds.constructor) .with (OpenAPI);
-
-// require('./lib/compile/api').registerCompileTargets()
-const cds = require('@sap/cds')
 require('./lib/compile/api').registerCompileTargets()
