22b338c51d9cd6940f9b4f97d451155f2dd26a7c
Daniel O'Grady <daniel.o-grady@sap.com>
2025-08-21 15:38:00 +0200

    Remove comments

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,23 +1,5 @@
 import js from '@eslint/js';
 import globals from 'globals'
-/*
-export default [
-  js.config({
-    env: {
-      jest: true,
-      es6: true,
-      node: true,
-    },
-    parserOptions: {
-      ecmaVersion: 'latest',
-    },
-    rules: {
-      'no-unused-vars': ['warn', { argsIgnorePattern: 'lazy' }],
-      'no-extra-semi': 1,
-    },
-  }),
-];
-*/
 
 export default [
     js.configs.recommended,
