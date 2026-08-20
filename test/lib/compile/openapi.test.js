8b037a6a9f0ee42cf048ec37a73020c69074c9f9
Lukas Schwabe <lukas.schwabe@amag.ch>
2026-08-20 16:42:43 +0200

    fix: use OData V4 path for OpenAPI server URL if no protocol is set to match @sap/cds default behavior
    Signed-off-by: Lukas Schwabe <lukas.schwabe@amag.ch>

--- test/lib/compile/openapi.test.js
+++ test/lib/compile/openapi.test.js
@@ -262,6 +262,21 @@ service CatalogService {
     assert(openapi.servers[0].url.includes('odata'));
   });
 
+  test('default service without @protocol renders server URL with odata/v4', () => {
+    const csn = cds.compile.to.csn(`
+      @path: 'catalog'
+      service CatalogService {
+        entity Books {
+          key ID : Integer;
+          title   : String;
+        }
+      }`
+    );
+    const openapi = toOpenApi(csn);
+    assert(openapi.servers[0].url.includes('odata/v4'));
+    assert(!openapi.servers[0].url.includes('rest'));
+  });
+
   test('options: Multiple servers', () => {
     const csn = cds.compile.to.csn(`
       service A {entity E { key ID : UUID; };};`
